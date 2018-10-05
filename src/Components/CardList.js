07fa4ec13fd7c59a53845907bb16cd379e5c03d7
Nora <norapuskas@yahoo.com>
2018-10-05 19:01:56 +0200

    refact(robofriends): refactor CardList component

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardsArray =  robots.map((robot, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-  })
   return (
     <div>
-      {cardsArray}
+      {robots.map((robot, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
   );
 };
