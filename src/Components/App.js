d422de7cbb65d42141a7d9f674637940dd584a57
Nora <norapuskas@yahoo.com>
2018-10-07 17:56:52 +0200

    feat(robofriends): add functionality to the searchbox, add state to app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from '../robots';
 
-const App = () => (
-  <div className="tc">
-    <h1>ROBOFRIENDS</h1>
-    <SearchBox />
-    <CardList robots={robots} />
-  </div>
-);
+class App extends Component {
+  state = {
+    robots: robots,
+    searchfield: '',
+  };
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>ROBOFRIENDS</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
