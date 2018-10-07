5a44eec875a1553583b732a9678c32a8b89f6c03
Nora <norapuskas@yahoo.com>
2018-10-07 20:41:21 +0200

    feat(robofriends): fetch users from jsonplaceholder

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import CardList from '../CardList';
 import SearchBox from '../SearchBox';
-import { robots } from '../../robots';
 import './App.css';
 
 class App extends Component {
   state = {
-    robots: robots,
+    robots: [],
     searchfield: '',
   };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+      response.json().then((users) => {
+        this.setState({});
+      });
+    });
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -20,13 +27,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="main-title">ROBOFRIENDS</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="main-title tc">Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="main-title">ROBOFRIENDS</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
