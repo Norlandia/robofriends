5dd4fa3c1b23f0596636b3a63742cdc8b06c4d56
Nora <norapuskas@yahoo.com>
2018-10-07 18:36:06 +0200

    feat(robofriends): add styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from '../robots';
+import CardList from '../CardList';
+import SearchBox from '../SearchBox';
+import { robots } from '../../robots';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -21,7 +22,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>ROBOFRIENDS</h1>
+        <h1 className="main-title">ROBOFRIENDS</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
