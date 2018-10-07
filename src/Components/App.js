0032573cb5fe7f050e3493e1e891963fa605fa19
Nora <norapuskas@yahoo.com>
2018-10-07 17:03:32 +0200

    feat(robofriends): Create title and SearchBox component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from '../robots';
 
-
 const App = () => (
- <CardList robots={robots}/>
+  <div className="tc">
+    <h1>ROBOFRIENDS</h1>
+    <SearchBox />
+    <CardList robots={robots} />
+  </div>
 );
 
 export default App;
