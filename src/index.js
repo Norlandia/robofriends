24d2ef36d0c4090ca04806d32a0404de2f37b643
Nora <norapuskas@yahoo.com>
2018-10-08 23:25:51 +0200

    feat(robofriends): create store, add provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './containers/App/App';
+import searchRobots from './reducers/reducers';
 import 'tachyons';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
